29e17f6dccb60568d3d3ca2708950c2ce42a62d2
Zaffere Shaffari <zaffere@Zafferes-MacBook-Pro.local>
2020-06-13 23:05:20 +0800

    First commit for my COVID-19 Stats Project

--- index.js
+++ index.js
@@ -62,9 +62,140 @@ app.post('/show', (req,res) => {
         const day = (response.data.response[0].day);
         const time = (response.data.response[0].time);
         console.log(response.data.response[0].time);
-        if (country === 'usa') {
-          country = 'USA';
+        if (country) {
+          country.toUpperCase();
         }
+        let widthDisplay;
+        let colorDisplay;
+        if (totalDeaths > 10000) {
+          widthDisplay = '100%';
+          colorDisplay = 'danger'
+        } else if (totalDeaths >= 5000 && totalDeaths < 10000) {
+          widthDisplay = '75%';
+          colorDisplay = 'warning';
+        } else if (totalDeaths >= 500 && totalDeaths < 5000) {
+          widthDisplay = '45%';
+          colorDisplay = 'warning';
+        } else if (totalDeaths > 10 && totalDeaths < 500) {
+          widthDisplay = '25%';
+          colorDisplay = 'success';
+        } else if (totalDeaths > 0 && totalDeaths < 10) {
+          widthDisplay = '10%';
+          colorDisplay = 'success';
+        } else {
+          widthDisplay = '0%';
+          colorDisplay = 'info';
+        };
+
+        let recentwidthDisplay;
+        let recentcolorDisplay;
+        if (newDeaths >= 10000) {
+          recentwidthDisplay = '100%';
+          recentcolorDisplay = 'danger'
+        } else if (newDeaths >= 5000 && newDeaths < 10000) {
+          recentwidthDisplay = '75%';
+          recentcolorDisplay = 'warning';
+        } else if (newDeaths >= 500 && newDeaths < 5000) {
+          recentwidthDisplay = '45%';
+          recentcolorDisplay = 'warning';
+        } else if (newDeaths > 10 && newDeaths < 500) {
+          recentwidthDisplay = '25%';
+          recentcolorDisplay = 'success';
+        } else if (newDeaths > 0 && newDeaths < 10) {
+          recentwidthDisplay = '10%';
+          recentcolorDisplay = 'success';
+        } else if (newDeaths == 'null' || newDeaths == 0) {
+          recentwidthDisplay = '0%';
+          recentcolorDisplay = 'info';
+        };
+
+        let newCaseswidthDisplay;
+        let newCasescolorDisplay;
+        if (newCases >= 10000) {
+          newCaseswidthDisplay = '100%';
+          newCasescolorDisplay = 'danger'
+        } else if (newCases >= 5000 && newCases < 10000) {
+          newCaseswidthDisplay = '75%';
+          newCasescolorDisplay = 'danger';
+        } else if (newCases >= 500 && newCases < 5000) {
+          newCaseswidthDisplay = '45%';
+          newCasescolorDisplay = 'danger';
+        } else if (newCases > 10 && newCases < 500) {
+          newCaseswidthDisplay = '25%';
+          newCasescolorDisplay = 'warning';
+        } else if (newCases > 0 && newCases < 10) {
+          newCaseswidthDisplay = '10%';
+          newCasescolorDisplay = 'success';
+        } else if (newCases == 'null' || newCases == 0) {
+          newCaseswidthDisplay = '0%';
+          newCasescolorDisplay = 'info';
+        };
+
+        let activeCaseswidthDisplay;
+        let activeCasescolorDisplay;
+        if (activeCases > 10000) {
+          activeCaseswidthDisplay = '100%';
+          activeCasescolorDisplay = 'danger'
+        } else if (activeCases >= 5000 && activeCases < 10000) {
+          activeCaseswidthDisplay = '75%';
+          activeCasescolorDisplay = 'danger';
+        } else if (activeCases >= 500 && activeCases < 5000) {
+          activeCaseswidthDisplay = '45%';
+          activeCasescolorDisplay = 'warning';
+        } else if (activeCases > 10 && activeCases < 500) {
+          activeCaseswidthDisplay = '25%';
+          activeCasescolorDisplay = 'success';
+        } else if (activeCases > 0 && activeCases < 10) {
+          activeCaseswidthDisplay = '10%';
+          activeCasescolorDisplay = 'success';
+        } else {
+          activeCaseswidthDisplay = '0%';
+          activeCasescolorDisplay = 'info';
+        };
+
+        let criticalCaseswidthDisplay;
+        let criticalCasescolorDisplay;
+        if (totalDeaths > 10000) {
+          criticalCaseswidthDisplay = '100%';
+          criticalCasescolorDisplay = 'danger'
+        } else if (criticalCases >= 5000 && criticalCases < 10000) {
+          criticalCaseswidthDisplay = '75%';
+          criticalCasescolorDisplay = 'warning';
+        } else if (criticalCases >= 500 && criticalCases < 5000) {
+          criticalCaseswidthDisplay = '45%';
+          criticalCasescolorDisplay = 'warning';
+        } else if (criticalCases > 10 && criticalCases < 500) {
+          criticalCaseswidthDisplay = '25%';
+          criticalCasescolorDisplay = 'success';
+        } else if (criticalCases > 0 && criticalCases < 10) {
+          criticalCaseswidthDisplay = '10%';
+          criticalCasescolorDisplay = 'success';
+        } else {
+          criticalCaseswidthDisplay = '0%';
+          criticalCasescolorDisplay = 'info';
+        };
+
+        let recoveredCaseswidthDisplay;
+        let recoveredCasescolorDisplay;
+        if (recoveredCases > 10000) {
+          recoveredCaseswidthDisplay = '100%';
+          recoveredCasescolorDisplay = 'success'
+        } else if (recoveredCases >= 5000 && recoveredCases < 10000) {
+          recoveredCaseswidthDisplay = '75%';
+          recoveredCasescolorDisplay = 'success';
+        } else if (recoveredCases >= 500 && recoveredCases < 5000) {
+          recoveredCaseswidthDisplay = '45%';
+          recoveredCasescolorDisplay = 'success';
+        } else if (recoveredCases > 10 && recoveredCases < 500) {
+          recoveredCaseswidthDisplay = '25%';
+          recoveredCasescolorDisplay = 'success';
+        } else if (recoveredCases > 0 && recoveredCases < 10) {
+          recoveredCaseswidthDisplay = '10%';
+          recoveredCasescolorDisplay = 'success';
+        } else {
+          recoveredCaseswidthDisplay = '0%';
+          recoveredCasescolorDisplay = 'info';
+        };
 
         res.send(`
 
@@ -84,6 +215,7 @@ app.post('/show', (req,res) => {
               <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.13.0/css/all.css" integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V" crossorigin="anonymous">
 
               <title>COVID-19 | Reports</title>
+
             </head>
             <body class="">
 
@@ -114,10 +246,16 @@ app.post('/show', (req,res) => {
             <main role="main" class="inner cover text-center">
               <div class="card rounded-lg p-3">
               <h1 class="text-capitalize">Deaths</h1>
-              <p>Total Deaths</p>
+              <p>Total Death<p>
+              <div class="progress rounded" style="height: 10px">
+                <div class="progress-bar bg-${colorDisplay}" style="width:${widthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
               <small>${totalDeaths}</small>
               <hr>
               <p>Recent Deaths</p>
+              <div class="progress rounded" style="height: 10px">
+                <div class="progress-bar bg-${recentcolorDisplay}" style="width:${recentwidthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
               <small>${newDeaths}</small>
               <hr>
 
@@ -145,15 +283,27 @@ app.post('/show', (req,res) => {
                   <div class="card rounded-lg p-3">
                   <h1 class="text-capitalize">Cases</h1>
                   <p>New Cases</p>
+                  <div class="progress rounded" style="height: 10px">
+                    <div class="progress-bar bg-${newCasescolorDisplay}" style="width:${newCaseswidthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
                   <small>${newCases}</small>
                   <hr>
                   <p>Active Cases</p>
+                  <div class="progress rounded" style="height: 10px">
+                    <div class="progress-bar bg-${activeCasescolorDisplay}" style="width:${activeCaseswidthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
                   <small>${activeCases}</small>
                   <hr>
                   <p>Critical Cases</p>
+                  <div class="progress rounded" style="height: 10px">
+                    <div class="progress-bar bg-${criticalCasescolorDisplay}" style="width:${criticalCaseswidthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
                   <small>${criticalCases}</small>
                   <hr>
                   <p>Recovered Cases</p>
+                  <div class="progress rounded" style="height: 10px">
+                    <div class="progress-bar bg-${recoveredCasescolorDisplay}" style="width:${recoveredCaseswidthDisplay}" role="progressbar" aria-valuenow="25%" aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
                   <small>${recoveredCases}</small>
                   <hr>
 
@@ -166,6 +316,9 @@ app.post('/show', (req,res) => {
 
               <div class="col-lg-6">
                   <main role="main" class="inner cover text-center">
+
+
+
 
                     <div class="card rounded-lg p-3">
                       <h1>Country</h1>
@@ -196,6 +349,7 @@ app.post('/show', (req,res) => {
           <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
           <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
           <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
         </body>
       </html>
 
@@ -205,6 +359,7 @@ app.post('/show', (req,res) => {
       })
 
       .catch((error)=>{
+        console.log(error);
         res.sendFile(__dirname + '/error.html');
       });
 });
